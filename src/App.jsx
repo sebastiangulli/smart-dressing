6f647ddd5187533cef44f680c4784007dd74d6fb
sebastiagulli <sebastiangulli@outlook.com>
2024-09-09 19:24:38 -0300

    add main classes, add rubik font license and modify theme colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import '../public/styles/main.css'
 import '../public/styles/fonts.css'
+import '../public/styles/classes.css'
 import { ThemeProvider } from "./contexts/ThemeContext"
 
 export const App = () => {
