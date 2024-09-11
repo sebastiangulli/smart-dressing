ee683748c9b139b3556bd91142106be367770379
sebastiagulli <sebastiangulli@outlook.com>
2024-09-11 12:21:41 -0300

    add styles, add theme classes and add images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,35 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { ThemeContext } from "../contexts/ThemeContext"
+import '../../public/styles/hero.css'
 
-const categories = ['Zapatos', 'Trajes', 'Camisas', 'Corbatas y Moños', 'Chalecos', 'Abrigos', 'Accesorios']
+const categories = ['Zapatos', 'Trajes', 'Camisas', 'Corbatas y Moños', 'Chalecos', 'Abrigos', 'Accesorios y Joyería']
 
 export const Hero = () => {
     const { theme } = useContext(ThemeContext)
+    const [photo, setPhoto] = useState(1)
+    useEffect(() => {
+        const photoInterval = setInterval(() => setPhoto(prevPhoto => (prevPhoto % 3) + 1), 2000);
+        return () => clearInterval(photoInterval);
+    }, [])
     return (
         <main className={`hero ${theme}-mode-bg`}>
             <div className="hero__container section-container">
-                <div className="hero__container__left-box">
-                    <span className="hero__container__left-box__callout">
+                <div className="hero__container__left-box hero-box">
+                    <span className={`hero__container__left-box__callout hero-callout ${theme}-mode-text`}>
                         Todo lo que necesitas para verte bien
                     </span>
-                    <div className="hero__container__left-box__categories">
+                    <span className={`hero__container__left-box__quote hero-qoute ${theme}-mode-text`}>
+                        <span className="hero__container__left-box__quote__text hero-quote-text">
+                            "El mejor modo de vestir, una inversión para tu imagen"
+                        </span>
+                        <span className="hero__container__left-box__quote__author hero-quote-author">
+                            (Sebastián Gulli, CEO de SD)
+                        </span>
+                    </span>
+                    <div className="hero__container__left-box__categories hero-categories">
                         {
                             categories.map((category, index) =>
-                                <button className="hero__container__left-box__categories__button"
+                                <button className={`hero__container__left-box__categories__button category-button ${theme}-mode-border ${theme}-mode-text ${theme}-mode-bg`}
                                     key={index}>
                                     {category}
                                 </button>
@@ -23,8 +37,9 @@ export const Hero = () => {
                         }
                     </div>
                 </div>
-                <div className="hero__container__right-box">
-                    <img className="hero__container__right-box__product" src="" alt="" />
+                <div className="hero__container__right-box hero-box">
+                    <img className="hero__container__right-box__image hero-image not-selectable"
+                        src={`../../public/images/hero/smart-man-${photo}.webp`} alt="Foto de hombre elegante" />
                 </div>
             </div>
         </main>
